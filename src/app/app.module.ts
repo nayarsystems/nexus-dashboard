34a16f88556e2094760159f7792d22dbd88eed09
Oriol Flors Mas <oriol.flors.mas@gmail.com>
2016-11-12 18:58:15 +0100

    Angular material first working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { routing, appRoutingProviders } from './app.routes';
 
@@ -11,17 +12,20 @@ import { LoggedInGuard } from './guards/logged-in.guard';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    WrapperComponent
   ],
   imports: [
     routing,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [
     appRoutingProviders,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
 import { LoggedInGuard } from './guards/logged-in.guard';
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
+  { path: 'dashboard', component: WrapperComponent }
 ];
 
 export const appRoutingProviders: any[] = [
